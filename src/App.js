f8ce7da0a39a667f5746507b858507774342b16d
Maxime Féry <maxime.fery@outlook.fr>
2020-01-29 16:01:15 +0100

    create AboutUs.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import Performances from './components/Performances'
+import AboutUs from './components/AboutUs'
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Performances />
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutUs.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div className="About-Us">
+        <h2><a id="About_Us"></a>About Us</h2>
+        
+        <p>At the dawn of the 1980s, a troupe of artists founded by Gilles Leblanc demonstrated his talents in the streets of Baie-Saint-Paul,
+            a charming village nestled on the shores of the St.Lawrence River near the city from Quebec.
+            Known to the public as The jugglers of Baie-Saint-Paul, this troupe will form the embryo of what would become the Wild Circus.
+            This troupe, which gave an eclectic show with jugglers, dancers, fire-eaters and musicians, charmed the audience of the region with his show. These early admirers and artists were far from suspecting that from these beginnings would emerge something magical, exciting and revolutionary that would be reflected on a global scale.
+        </p>
+    </div>
+  );
+}
+
+export default AboutUs;
