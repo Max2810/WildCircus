e2680c1e5c91668ca0994dd54d73379046aefb71
Maxime Féry <maxime.fery@outlook.fr>
2020-01-29 16:07:40 +0100

    create Prices.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header'
 import Performances from './components/Performances'
 import AboutUs from './components/AboutUs'
+import Prices from './components/Prices'
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Performances />
       <AboutUs />
+      <Prices />
     </div>
   );
 }
