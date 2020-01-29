7965b9591019844ad929c0a807bfe62f7273725b
Maxime Féry <maxime.fery@outlook.fr>
2020-01-29 14:45:42 +0100

    adjust css on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import React from 'react'
-
+import './Header.css'
 
 function Header() {
   return (
     <div className="Header">
-      <header class="Haut">
+      <header className="Haut">
         <h1>Wild Circus</h1>
-        <nav class="menu">
+        <nav className="menu">
           <ul>
-            <li> | <a href="#Performances">Performances</a> | </li>
-            <li><a href="#About_Us">About Us</a> |</li>
-            <li><a href="#Prices">Prices</a> |</li>
-            <li><a href="#Contact_Us">Contact</a> |</li>
+            <li><a href="#Performances">Performances</a></li>
+            <li><a href="#About_Us">About Us</a></li>
+            <li><a href="#Prices">Prices</a></li>
+            <li><a href="#Contact_Us">Contact</a></li>
           </ul>
         </nav>
       </header>
