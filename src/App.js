2da1361de3dae655712d02ff67bb2e69af41870c
Maxime Féry <maxime.fery@outlook.fr>
2020-01-29 15:50:28 +0100

    create Performances.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header'
+import Performances from './components/Performances'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Performances />
     </div>
   );
 }
